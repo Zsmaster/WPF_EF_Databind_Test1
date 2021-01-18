3585d58291650723c01deb1cfd60ee3308b46db3
Zsolt Kardos <zsolt.kardos@hotmail.hu>
2021-01-18 07:22:19 +0100

    First commit

---
+++ WpfApp2/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using WpfApp2.ViewModel;
+
+namespace WpfApp2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private UserViewModel user = new UserViewModel();
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.DataContext = user;
+        }
+
+        private void btnSave_Click(object sender, RoutedEventArgs e)
+        {
+            user.AddUser();
+        }
+    }
+}
---
+++ WpfApp2/ViewModel/UserViewModel.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WpfApp2.Data;
+
+namespace WpfApp2.ViewModel
+{
+    class UserViewModel : INotifyPropertyChanged
+    {
+        private myPCWebShopEntities entities = new myPCWebShopEntities();
+        private User _user = new User();
+
+        public string UserNameNotify
+        {
+            get 
+            {
+                return _user.UserName;
+            }
+
+            set 
+            {
+                _user.UserName = value;
+                OnPropertyChanged("UserNameNotify");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null) handler(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public void AddUser()
+        {
+            _user.Id = "1";
+            _user.Registrated = DateTime.Now;
+            _user.LastLogin = DateTime.Now;
+            _user.EmailConfirmed = true;
+            _user.PhoneNumberConfirmed = true;
+            _user.TwoFactorEnabled = false;
+            _user.LockoutEnabled = false;
+            _user.AccessFailedCount = 0;
+            entities.User.Add(_user);
+            entities.SaveChanges();
+        }
+    }
+}
